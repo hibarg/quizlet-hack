3d48f2b2a6b2608cd1c5ca947fea01ad4354cab7
glixzzy <itrxpzz0@gmail.com>
2022-01-28 14:09:08 -0600

    format code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
-import { GameInfo } from './types'
+import { GameInfo } from "./types";
 
 const getQuestionBoxes = () => {
   const questionBoxes = document.querySelectorAll(".StudentAnswerOptions");
 
   if (!questionBoxes) {
     throw new Error("Can't find the question box element!");
-  };
+  }
 
   return questionBoxes;
 };
 
 const getGameData = async (gameCode: string | null) => {
-    const response = await fetch(`https://quizlet.com/webapi/3.2/game-instances?filters={"gameCode":${gameCode},"isInProgress":true,"isDeleted":false}&perPage=500`)
-    const data: GameInfo = await response.json();
+  const response = await fetch(
+    `https://quizlet.com/webapi/3.2/game-instances?filters={"gameCode":${gameCode},"isInProgress":true,"isDeleted":false}&perPage=500`
+  );
+  const data: GameInfo = await response.json();
 
-    return data
-}
+  return data;
+};
 
 (async () => {
-    alert(`Created by gliz \n\nPress E to auto answer!`);
+  alert(`Created by gliz \n\nPress E to auto answer!`);
 
-    const gameCode = prompt('Enter Game Code: ');
+  const gameCode = prompt("Enter Game Code: ");
 
-    const gameData = await getGameData(gameCode);
+  const gameData = await getGameData(gameCode);
 
-    const itemId = gameData.responses[0].models.gameInstance[0].itemId;
+  const itemId = gameData.responses[0].models.gameInstance[0].itemId;
 
-    fetch('https://quizlet.com/' + itemId).then(e => e.text()).then(t => console.log(t));
+  fetch("https://quizlet.com/" + itemId)
+    .then((e) => e.text())
+    .then((t) => console.log(t));
 })();
